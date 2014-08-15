f4398b9fe4638a6347a15efcf1b3e6dbd122a3e0
Głowne repozytorium kodu <jacek.rawiak@hotmail.com>
2014-08-15 15:26:13 +0200

    zmiana obsługi klienta poczty
    rezygnacja z sendgrid

--- stafix24.pl/Services/MailService.cs
+++ stafix24.pl/Services/MailService.cs
@@ -12,45 +12,81 @@ namespace stafix24.pl.Services
 {
     public class MailService : IMailService
     {
+
         public bool SendMail(string from, string to, string subject, string body)
         {
-
-            try
-            {
-            MailMessage mailMsg = new MailMessage();
-
-            // To
-            mailMsg.To.Add(new MailAddress(to.ToString(), "Administrator witryny STAFix24.pl"));
-
-            // From
-            mailMsg.From = new MailAddress(from.ToString(), "Witryna STAFix24.pl");
-
-            // Subject and multipart/alternative Body
-            mailMsg.Subject = subject.ToString();
-            string text = body;
-            string html = @"<p>"+body+"</p>";
-            //mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
-            mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+            MailMessage msg = new MailMessage(from, to);
+            msg.Subject = subject;
+            msg.Body = body;
+            msg.BodyEncoding = Encoding.UTF8;
+            msg.IsBodyHtml = true;
 
             //get config params
             string sMTPServerName = WebConfigurationManager.AppSettings["SMTPServerName"].ToString();
             string sMTPLoginName = WebConfigurationManager.AppSettings["SMTPLoginName"].ToString();
             string sMTPPassword = WebConfigurationManager.AppSettings["SMTPPassword"].ToString();
 
-            // Init SmtpClient and send
-            SmtpClient smtpClient = new SmtpClient(sMTPServerName, Convert.ToInt32(587));
-            System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(sMTPLoginName, sMTPPassword);
-            smtpClient.Credentials = credentials;
 
-            smtpClient.Send(mailMsg);
+            SmtpClient client = new SmtpClient(sMTPServerName, 587);
+            System.Net.NetworkCredential basicCredential = new System.Net.NetworkCredential(sMTPLoginName, sMTPPassword);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = true;
+            client.Credentials = basicCredential;
+            try
+            {
+                client.Send(msg);
             }
             catch (Exception)
             {
-            return false;
+                return false;
             }
 
             return true;
-
         }
+
+
+
+        // obsługa SendGrid
+
+        //public bool SendMail(string from, string to, string subject, string body)
+        //{
+
+        //    try
+        //    {
+        //    MailMessage mailMsg = new MailMessage();
+
+        //    // To
+        //    mailMsg.To.Add(new MailAddress(to.ToString(), "Administrator witryny STAFix24.pl"));
+
+        //    // From
+        //    mailMsg.From = new MailAddress(from.ToString(), "Witryna STAFix24.pl");
+
+        //    // Subject and multipart/alternative Body
+        //    mailMsg.Subject = subject.ToString();
+        //    string text = body;
+        //    string html = @"<p>"+body+"</p>";
+        //    //mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+        //    mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+
+        //    //get config params
+        //    string sMTPServerName = WebConfigurationManager.AppSettings["SMTPServerName"].ToString();
+        //    string sMTPLoginName = WebConfigurationManager.AppSettings["SMTPLoginName"].ToString();
+        //    string sMTPPassword = WebConfigurationManager.AppSettings["SMTPPassword"].ToString();
+
+        //    // Init SmtpClient and send
+        //    SmtpClient smtpClient = new SmtpClient(sMTPServerName, Convert.ToInt32(587));
+        //    System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(sMTPLoginName, sMTPPassword);
+        //    smtpClient.Credentials = credentials;
+
+        //    smtpClient.Send(mailMsg);
+        //    }
+        //    catch (Exception)
+        //    {
+        //    return false;
+        //    }
+
+        //    return true;
+
+        //}
     }
 }
